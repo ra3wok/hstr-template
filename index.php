5882c4b2a3ff39c1ffcfe8257f7aeaede4f2d12c
Alex Dolzhenkov <ra3wok@gmail.com>
2021-03-26 16:35:57 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="ru-RU">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="shortcut icon" href="" type="image/x-icon" />
+
+    <title>Валидный шаблон 2021</title>
+    <meta name="description" content="Валидный шаблон 2021" />
+    <meta name="robots" content="index, follow" />
+    <link rel="canonical" href="#" />
+
+    <link rel='dns-prefetch' href='//s.w.org' />
+    <link rel='stylesheet' id='hstr-style-css'  href='styles.css' type='text/css' media='all' />
+</head>
+
+<body>
+
+<div id="page" class="site">
+
+
+    <header class="hstr-header">
+        <div class="wrapper">
+            <div class="site-branding">
+
+                <div class="site-title"><a href="#" rel="home">Название сайта</a></div>
+
+                <div class="site-description">Описание сайта</div>
+
+            </div><!-- .site-branding -->
+
+            <nav id="site-navigation" class="main-navigation">
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Меню</button>
+                <div class="menu-header-menu-container"><ul id="primary-menu" class="menu"><li id="menu-item-543" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-543"><a href="contacts/">Контакты</a></li>
+                    </ul></div>		</nav><!-- #site-navigation -->
+        </div>
+    </header><!-- /header -->
+
+
+
+    <div id="content" class="hstr-content">
+
+        <div id="primary" class="content-area wrapper">
+
+
+            <main id="main" class="hstr-main">
+
+
+
+
+                Содержание основной секции
+
+
+
+                <nav class="navigation pagination" role="navigation" aria-label="Записи">
+                    <h2 class="screen-reader-text">Навигация по записям</h2>
+                    <div class="nav-links"><span aria-current="page" class="page-numbers current">1</span>
+                        <a class="page-numbers" href="page/2/">2</a>
+                        <a class="next page-numbers" href="page/2/">-></a></div>
+                </nav>
+
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+
+
+        <aside id="secondary" class="hstr-aside">
+            <section id="text-4" class="widget widget_text">			<div class="textwidget"></div>
+
+                Содержание вторичной секции
+            </section>
+
+        </aside><!-- #secondary -->
+    </div>
+</div> <!-- #content -->
+
+
+
+<footer class="hstr-footer">
+    <div class="wrapper">
+        <div class="site-info">
+            <?echo "&#169; " . date('Y').", "; ?> <a href="#" >Имя сайта</a> ,
+            <a href="#">Карта сайта</a>
+        </div><!-- .site-info -->
+    </div>
+</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+<script type="text/javascript" src="scripts.js"></script>
+</body>
+</html>
