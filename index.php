9a6966a0624880e5f1f32d09b9653cdd3cbb89b4
Alex Dolzhenkov <ra3wok@gmail.com>
2021-04-28 18:18:26 +0300

    28.04

--- index.php
+++ index.php
@@ -3,33 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-
     <title>Валидный шаблон 2021</title>
     <meta name="description" content="Валидный шаблон 2021" />
     <meta name="robots" content="index, follow" />
     <link rel="canonical" href="#" />
-    <link rel='stylesheet' id='hstr-style-css'  href='css/main.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/main.css' type='text/css' media='all' />
 </head>
 
 <body>
 
-<div id="page" class="site">
+<div class="hstr-site">
 
 
     <header class="hstr-header">
         <div class="wrapper">
-            <div class="site-branding">
+            <div class="hstr-site-branding">
 
-                <div class="site-title"><a href="#" rel="home">Название сайта</a></div>
+                <div class="hstr-site-branding__title"><a href="#" rel="home">Название сайта</a></div>
 
-                <div class="site-description">Описание сайта</div>
+                <div class="hstr-site-branding__description">Описание сайта</div>
 
-            </div><!-- .site-branding -->
+            </div><!-- .hstr-site-branding -->
 
             <nav id="site-navigation" class="main-navigation">
-                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Меню</button>
+                <button class="site-menu-toggle" aria-controls="primary-menu" aria-expanded="false" >Меню</button>
                 <div class="menu-header-menu-container"><ul id="primary-menu" class="menu"><li id="menu-item-543" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-543"><a href="contacts/">Контакты</a></li>
                     </ul></div>		</nav><!-- #site-navigation -->
         </div>
@@ -37,48 +35,60 @@
 
 
 
-    <div id="content" class="hstr-content">
-
-        <div id="primary" class="content-area wrapper">
+    <div class="hstr-content">
 
 
-            <main id="main" class="hstr-main">
+        <div class="wrapper">
+
+            <main class="hstr-main">
 
 
 
+                <h1>Заголовок 1</h1>
 
-                Содержание основной секции
+                <p>Содержание основной секции Содержание основной секции Содержание основной секции Содержание основной секции </p>
 
+                <h2>Заголовок 2</h2>
+
+                <p>Содержание основной секции Содержание основной секции Содержание основной секции Содержание основной секции </p>
+
+                <h3>Заголовок 3</h3>
+
+                <p>Содержание основной секции Содержание основной секции Содержание основной секции Содержание основной секции </p>
+
+
+
+                <p><a href="#" class="hstr-button">Кнопка</a></p>
+
+                <br/><br/><br/>
 
 
                 <nav class="navigation pagination" role="navigation" aria-label="Записи">
-                    <h2 class="screen-reader-text">Навигация по записям</h2>
-                    <div class="nav-links"><span aria-current="page" class="page-numbers current">1</span>
-                        <a class="page-numbers" href="page/2/">2</a>
+                    <div class="screen-reader-text">Навигация по записям</div>
+                    <div class="nav-links">
+                        <span class="hstr-button current">1</span>
+                        <a class="hstr-button" href="page/2/">2</a>
                         <a class="next page-numbers" href="page/2/">-></a></div>
                 </nav>
 
 
             </main><!-- #main -->
-        </div><!-- #primary -->
 
-
-        <aside id="secondary" class="hstr-aside">
+        <aside class="hstr-aside">
             <section id="text-4" class="widget widget_text">			<div class="textwidget"></div>
-
                 Содержание вторичной секции
             </section>
 
         </aside><!-- #secondary -->
-    </div>
-</div> <!-- #content -->
+        </div>
+    </div> <!-- #content -->
 
 
 
 <footer class="hstr-footer">
     <div class="wrapper">
         <div class="site-info">
-            <?php echo "&#169;" . date('Y').", "; ?> <a href="#" >Имя сайта</a> ,
+            <?php echo "&#169; " . date('Y').", "; ?> <a href="#" >Имя сайта</a> ,
             <a href="#">Карта сайта</a>
         </div><!-- .site-info -->
     </div>
@@ -86,7 +96,7 @@
 
 </div><!-- #page -->
 
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+<script type="text/javascript" src="js/jQuery3.js"></script>
 <script type="text/javascript" src="js/base.js"></script>
 </body>
 </html>
