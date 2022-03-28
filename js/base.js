18157d2bd12788773d6fe44de6041a3dc52ed6ce
Alex Dolzhenkov <ra3wok@gmail.com>
2022-03-28 09:44:14 +0300

    28.03

--- js/base.js
+++ js/base.js
@@ -3,4 +3,12 @@ $(document).ready(function () {
 
     $(".hstr-footer").addClass("loaded");
 
+    $(".menu-item").click(function() {
+        //console.log($(this)[0].firstChild.id);
+
+        var page_number = $( "li" ).index( this );
+        $( ".hstr-content" ).css("display" , "none");
+        $( "#page" + page_number ).css("display" , "block");
+    });
+
 });
